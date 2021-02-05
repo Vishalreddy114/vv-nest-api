4bf432ddbf166aee72a1bdee0d6e31792e102239
vishal Reddy <vishalreddy0507@gmail.com>
2021-02-05 10:01:39 -0600

    added user entity and also .gitignore

---
+++ src/location/entities/location.entity.ts
@@ -0,0 +1,17 @@
+export class Location {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Location {
+    @PrimaryGeneratedColumn()
+id: number;
+
+    @Column()
+    latitude: number;
+
+    @Column()
+    longitude: number;
+
+    @Column({ default: true })
+    isActive: boolean;
+}
